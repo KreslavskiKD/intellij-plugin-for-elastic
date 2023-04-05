91ba9c4c55e065acf5eaa157057a7d5092151720
KreslavskiKD <kirill.kreslavski@gmail.com>
2023-04-05 20:14:33 +0300

    first attempt in writing http requests to kibana

--- src/main/kotlin/com/github/kreslavskikd/intellijpluginforelastic/toolWindow/KibanaToolWindowFactory.kt
+++ src/main/kotlin/com/github/kreslavskikd/intellijpluginforelastic/toolWindow/KibanaToolWindowFactory.kt
@@ -1,7 +1,6 @@
 package com.github.kreslavskikd.intellijpluginforelastic.toolWindow
 
 import com.intellij.openapi.components.service
-import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
@@ -9,39 +8,36 @@ import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
 import com.github.kreslavskikd.intellijpluginforelastic.MyBundle
-import com.github.kreslavskikd.intellijpluginforelastic.services.MyProjectService
+import com.github.kreslavskikd.intellijpluginforelastic.services.KibanaProjectService
 import javax.swing.JButton
 
 
-class MyToolWindowFactory : ToolWindowFactory {
-
-    init {
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
-    }
+class KibanaToolWindowFactory : ToolWindowFactory {
 
     private val contentFactory = ContentFactory.SERVICE.getInstance()
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val myToolWindow = MyToolWindow(toolWindow)
-        val content = contentFactory.createContent(myToolWindow.getContent(), null, false)
+        val kibanaToolWindow = KibanaToolWindow(toolWindow)
+        val content = contentFactory.createContent(kibanaToolWindow.getContent(), null, false)
         toolWindow.contentManager.addContent(content)
     }
 
     override fun shouldBeAvailable(project: Project) = true
 
-    class MyToolWindow(toolWindow: ToolWindow) {
+    class KibanaToolWindow(toolWindow: ToolWindow) {
 
-        private val service = toolWindow.project.service<MyProjectService>()
+        private val service = toolWindow.project.service<KibanaProjectService>()
 
         fun getContent() = JBPanel<JBPanel<*>>().apply {
-            val label = JBLabel(MyBundle.message("randomLabel", "?"))
+            val label = JBLabel(MyBundle.message("label", "?"))
 
-            add(label)
-            add(JButton(MyBundle.message("shuffle")).apply {
+            add(JButton(MyBundle.message("button_send_request")).apply {
                 addActionListener {
-                    label.text = MyBundle.message("randomLabel", service.getRandomNumber())
+                    label.text = MyBundle.message("label", service.sendKibanaRequest())
                 }
             })
+            add(label)
+
         }
     }
 }
